a788b81721b528c634c56137d81f30ce46a5513e
Mohammad AlQanneh <mqanneh@gmail.com>
2017-12-28 13:40:43 +0200

    Added new composer script to clean .git directories.

---
+++ scripts/composer/ScriptHandler.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Seeds\composer;
+
+/**
+ * Seeds Composer Script Handler.
+ */
+class ScriptHandler {
+
+  /**
+   * Get Drupal root directory.
+   *
+   * @param string $root
+   *    Project root directory.
+   *
+   * @return string
+   *    Drupal root path.
+   */
+  protected static function getDrupalRoot($root) {
+    return $root . '/public_html';
+  }
+
+  /**
+   * Remove .git folder from modules, themes, profiles of development branches.
+   */
+  public static function removeGitDirectories() {
+    $root = static::getDrupalRoot(getcwd());
+    exec('find ' . $root . ' -name \'.git\' | xargs rm -rf');
+  }
+
+}

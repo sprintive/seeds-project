da5aa8d76250f68d7b4215f28b8fbfaef2ecd6e1
Yahya Al Hamad <yahya@sprintive.com>
2021-08-02 17:27:02 +0300

    Fast404 whitelist settings and seeds version

--- assets/settings.fast404.php
+++ assets/settings.fast404.php
@@ -68,7 +68,7 @@ $settings['fast404_exts'] = '/^(?!robots).*\.(txt|png|gif|jpe?g|css|js|ico|swf|f
  *
  * Default value for this setting is FALSE.
  */
-# $settings['fast404_string_whitelisting'] = ['cdn/farfuture', '/advagg_'];
+$settings['fast404_string_whitelisting'] = ['system/files'];
 
 /**
  * When Fast 404 checks for missing file assets, it will return a response
